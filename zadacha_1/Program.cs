a01d5a3330640bbd77d71bafb26fc3459ff0fa58
OlgaT <oliunea-t2002@mail.ru>
2022-08-14 13:41:39 +0300

    add zadacha 1

---
+++ zadacha_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру 
+//этого числа. Например: 456 - 5; 782- 8; 918 - 1.
+Console.WriteLine("Введите трехзначное число");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+while(number>100)
+{
+    number=number/10;
+} 
+int result = number%10;
+{
+    Console.WriteLine(result);
+}
