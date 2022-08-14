1239a9f2cf1d77a301c837fef6491abe52ef604a
OlgaT <oliunea-t2002@mail.ru>
2022-08-14 14:36:01 +0300

    add zadacha 2

--- zadacha_1/Program.cs
+++ zadacha_1/Program.cs
@@ -3,6 +3,10 @@
 Console.WriteLine("Введите трехзначное число");
 string? numberString = Console.ReadLine();
 int number = int.Parse(numberString!);
+if (number<0)
+{
+    number=number*(-1);
+}
 while(number>100)
 {
     number=number/10;
---
+++ zadacha_2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей
+// цифры нет. Например: 645 - 5; 32679 - 6; 78 - третьей цифры нет.
+Console.WriteLine("Введите число");
+string? numberString = Console.ReadLine();
+int number = int.Parse(numberString!);
+
+if(number>99 && number<1000)
+{
+    Console.WriteLine(number%10);
+}
+if(number>999 && number<10000)
+{
+    Console.WriteLine((number/10)%10);
+}
+if(number>9999 && number<100000)
+{
+    Console.WriteLine((number/100)%10);
+}
+
+if(number<=99)
+{
+    Console.WriteLine("третьей цифры нет");
+}
